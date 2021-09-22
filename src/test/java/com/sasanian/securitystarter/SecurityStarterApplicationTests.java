8b743944031b0a5e6e2cbf3f412a708a59000d3f
esasanianno <ehsan.sasanianno@robart.cc>
2021-09-22 15:51:10 +0200

    init commit

---
+++ src/test/java/com/sasanian/securitystarter/SecurityStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasanian.securitystarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
